aa1968027305fa85fc982649f5556fe570dcdc25
Lucas <cestislife@gmail.com>
2020-08-30 20:38:22 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -51,18 +51,3 @@ if (theme === "dark") {
 	sessionStorage.setItem('theme', 'light');
 	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 }
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = Math.max(panel.scrollHeight, panel.offsetHeight, panel.clientHeight) + "px";
-    } 
-  });
-}
